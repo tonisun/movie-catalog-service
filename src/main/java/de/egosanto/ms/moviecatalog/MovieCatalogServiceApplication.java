9795d5d61b08269665ad47bd722701449cbec482
Toni Zeidler <toni.z@gmx.de>
2022-10-10 23:37:51 +0200

    rename package

--- src/main/java/de/egosanto/ms/moviecatalog/MovieCatalogServiceApplication.java
+++ src/main/java/de/egosanto/ms/moviecatalog/MovieCatalogServiceApplication.java
@@ -1,4 +1,4 @@
-package de.egosanto.ms.movie;
+package de.egosanto.ms.moviecatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
